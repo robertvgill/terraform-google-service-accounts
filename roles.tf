6126184215312c0d182cf6c42cac03c846e9fb47
Robert Gill <robertvgill@gmail.com>
2023-10-06 13:00:24 +0800

    Initial commit

--- roles.tf
+++ roles.tf
@@ -1,5 +1,7 @@
 resource "google_project_iam_member" "project_member_role" {
-  for_each = { for idx, sa in local.sa_roles_flattened : "${sa["account_id"]}_${sa["role"]}" => sa }
+  for_each = {
+    for idx, sa in local.sa_roles_flattened : "${sa["account_id"]}_${sa["role"]}" => sa
+  }
 
   project    = var.project_id
   role       = replace(each.value["role"], "{{PROJECT_ID}}", var.project_id)
